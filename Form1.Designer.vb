43c75cb5fdc8bebc5b2ce2df6429fb2ece1b9f3a
Jarreth <jarrethfleischer@gmail.com>
2023-03-05 21:57:56 +0800

    Update

---
+++ Form1.Designer.vb
@@ -0,0 +1,353 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.TBIg = New System.Windows.Forms.TextBox()
+        Me.TBFb = New System.Windows.Forms.TextBox()
+        Me.TBEmail = New System.Windows.Forms.TextBox()
+        Me.TBPhone = New System.Windows.Forms.TextBox()
+        Me.TBName = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Display = New System.Windows.Forms.Button()
+        Me.Reset = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.igout = New System.Windows.Forms.Label()
+        Me.fbout = New System.Windows.Forms.Label()
+        Me.mailout = New System.Windows.Forms.Label()
+        Me.phoneout = New System.Windows.Forms.Label()
+        Me.namaout = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.labelphone = New System.Windows.Forms.Label()
+        Me.labelname = New System.Windows.Forms.Label()
+        Me.VScrollBar1 = New System.Windows.Forms.VScrollBar()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(41, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(168, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "MY CONTACT DETAILS"
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.TBIg)
+        Me.Panel1.Controls.Add(Me.TBFb)
+        Me.Panel1.Controls.Add(Me.TBEmail)
+        Me.Panel1.Controls.Add(Me.TBPhone)
+        Me.Panel1.Controls.Add(Me.TBName)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Location = New System.Drawing.Point(38, 34)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(472, 206)
+        Me.Panel1.TabIndex = 1
+        '
+        'TBIg
+        '
+        Me.TBIg.Location = New System.Drawing.Point(148, 166)
+        Me.TBIg.Name = "TBIg"
+        Me.TBIg.Size = New System.Drawing.Size(296, 27)
+        Me.TBIg.TabIndex = 9
+        '
+        'TBFb
+        '
+        Me.TBFb.Location = New System.Drawing.Point(148, 125)
+        Me.TBFb.Name = "TBFb"
+        Me.TBFb.Size = New System.Drawing.Size(296, 27)
+        Me.TBFb.TabIndex = 8
+        '
+        'TBEmail
+        '
+        Me.TBEmail.Location = New System.Drawing.Point(149, 88)
+        Me.TBEmail.Name = "TBEmail"
+        Me.TBEmail.Size = New System.Drawing.Size(296, 27)
+        Me.TBEmail.TabIndex = 7
+        '
+        'TBPhone
+        '
+        Me.TBPhone.Location = New System.Drawing.Point(148, 53)
+        Me.TBPhone.Name = "TBPhone"
+        Me.TBPhone.Size = New System.Drawing.Size(296, 27)
+        Me.TBPhone.TabIndex = 6
+        '
+        'TBName
+        '
+        Me.TBName.Location = New System.Drawing.Point(149, 18)
+        Me.TBName.Name = "TBName"
+        Me.TBName.Size = New System.Drawing.Size(296, 27)
+        Me.TBName.TabIndex = 5
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(13, 169)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(130, 20)
+        Me.Label6.TabIndex = 4
+        Me.Label6.Text = "INSTAGRAM         :"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(13, 128)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(129, 20)
+        Me.Label5.TabIndex = 3
+        Me.Label5.Text = "FACEBOOK           :"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 91)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(130, 20)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "EMAIL                   :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(13, 53)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(129, 20)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "PHONE NUMBER :"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(13, 21)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(130, 20)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "NAME                   :"
+        '
+        'Display
+        '
+        Me.Display.Location = New System.Drawing.Point(115, 246)
+        Me.Display.Name = "Display"
+        Me.Display.Size = New System.Drawing.Size(94, 29)
+        Me.Display.TabIndex = 2
+        Me.Display.Text = "Display"
+        Me.Display.UseVisualStyleBackColor = True
+        '
+        'Reset
+        '
+        Me.Reset.Location = New System.Drawing.Point(224, 246)
+        Me.Reset.Name = "Reset"
+        Me.Reset.Size = New System.Drawing.Size(94, 29)
+        Me.Reset.TabIndex = 3
+        Me.Reset.Text = "Reset"
+        Me.Reset.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(339, 246)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(94, 29)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Exit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel2.Controls.Add(Me.igout)
+        Me.Panel2.Controls.Add(Me.fbout)
+        Me.Panel2.Controls.Add(Me.mailout)
+        Me.Panel2.Controls.Add(Me.phoneout)
+        Me.Panel2.Controls.Add(Me.namaout)
+        Me.Panel2.Controls.Add(Me.Label7)
+        Me.Panel2.Controls.Add(Me.Label8)
+        Me.Panel2.Controls.Add(Me.Label9)
+        Me.Panel2.Controls.Add(Me.labelphone)
+        Me.Panel2.Controls.Add(Me.labelname)
+        Me.Panel2.Controls.Add(Me.VScrollBar1)
+        Me.Panel2.Location = New System.Drawing.Point(38, 293)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(472, 173)
+        Me.Panel2.TabIndex = 5
+        '
+        'igout
+        '
+        Me.igout.AutoSize = True
+        Me.igout.Location = New System.Drawing.Point(157, 149)
+        Me.igout.Name = "igout"
+        Me.igout.Size = New System.Drawing.Size(0, 20)
+        Me.igout.TabIndex = 14
+        '
+        'fbout
+        '
+        Me.fbout.AutoSize = True
+        Me.fbout.Location = New System.Drawing.Point(159, 109)
+        Me.fbout.Name = "fbout"
+        Me.fbout.Size = New System.Drawing.Size(0, 20)
+        Me.fbout.TabIndex = 13
+        '
+        'mailout
+        '
+        Me.mailout.AutoSize = True
+        Me.mailout.Location = New System.Drawing.Point(158, 73)
+        Me.mailout.Name = "mailout"
+        Me.mailout.Size = New System.Drawing.Size(0, 20)
+        Me.mailout.TabIndex = 12
+        '
+        'phoneout
+        '
+        Me.phoneout.AutoSize = True
+        Me.phoneout.Location = New System.Drawing.Point(159, 39)
+        Me.phoneout.Name = "phoneout"
+        Me.phoneout.Size = New System.Drawing.Size(0, 20)
+        Me.phoneout.TabIndex = 11
+        '
+        'namaout
+        '
+        Me.namaout.AutoSize = True
+        Me.namaout.Location = New System.Drawing.Point(159, 7)
+        Me.namaout.Name = "namaout"
+        Me.namaout.Size = New System.Drawing.Size(0, 20)
+        Me.namaout.TabIndex = 10
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(12, 149)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(130, 20)
+        Me.Label7.TabIndex = 9
+        Me.Label7.Text = "INSTAGRAM         :"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(14, 109)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(129, 20)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "FACEBOOK           :"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(13, 73)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(130, 20)
+        Me.Label9.TabIndex = 7
+        Me.Label9.Text = "EMAIL                   :"
+        '
+        'labelphone
+        '
+        Me.labelphone.AutoSize = True
+        Me.labelphone.Location = New System.Drawing.Point(14, 39)
+        Me.labelphone.Name = "labelphone"
+        Me.labelphone.Size = New System.Drawing.Size(129, 20)
+        Me.labelphone.TabIndex = 6
+        Me.labelphone.Text = "PHONE NUMBER :"
+        '
+        'labelname
+        '
+        Me.labelname.AutoSize = True
+        Me.labelname.Location = New System.Drawing.Point(14, 7)
+        Me.labelname.Name = "labelname"
+        Me.labelname.Size = New System.Drawing.Size(130, 20)
+        Me.labelname.TabIndex = 5
+        Me.labelname.Text = "NAME                   :"
+        '
+        'VScrollBar1
+        '
+        Me.VScrollBar1.Location = New System.Drawing.Point(442, -2)
+        Me.VScrollBar1.Name = "VScrollBar1"
+        Me.VScrollBar1.Size = New System.Drawing.Size(26, 173)
+        Me.VScrollBar1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(544, 478)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Reset)
+        Me.Controls.Add(Me.Display)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Welcome To My Page"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents TBIg As TextBox
+    Friend WithEvents TBFb As TextBox
+    Friend WithEvents TBEmail As TextBox
+    Friend WithEvents TBPhone As TextBox
+    Friend WithEvents TBName As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Display As Button
+    Friend WithEvents Reset As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents VScrollBar1 As VScrollBar
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents labelphone As Label
+    Friend WithEvents labelname As Label
+    Friend WithEvents igout As Label
+    Friend WithEvents fbout As Label
+    Friend WithEvents mailout As Label
+    Friend WithEvents phoneout As Label
+    Friend WithEvents namaout As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,57 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Display.Click
+        namaout.Text = TBName.Text
+        phoneout.Text = TBPhone.Text
+        mailout.Text = TBEmail.Text
+        fbout.Text = TBFb.Text
+        igout.Text = TBIg.Text
+
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Close()
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Panel2_Paint(sender As Object, e As PaintEventArgs) Handles Panel2.Paint
+
+
+    End Sub
+
+    Private Sub TBName_TextChanged(sender As Object, e As EventArgs) Handles TBName.TextChanged
+
+    End Sub
+
+    Private Sub Label11_Click(sender As Object, e As EventArgs) Handles labelname.Click
+
+    End Sub
+
+    Private Sub Label10_Click(sender As Object, e As EventArgs) Handles labelphone.Click
+
+    End Sub
+
+    Private Sub Label9_Click(sender As Object, e As EventArgs) Handles Label9.Click
+
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
+
+    End Sub
+
+    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
+
+    End Sub
+
+    Private Sub Reset_Click(sender As Object, e As EventArgs) Handles Reset.Click
+        Controls.Clear()
+        InitializeComponent()
+
+    End Sub
+End Class
